dc868ad79fffa94d8d295d3d5681b552142d5123
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-05 10:31:45 +0200

    init tabView

--- ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUI/ContentView.swift
+++ ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUI/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
+enum AppScreen: Hashable, Identifiable, CaseIterable {
+    case backyard
+    case birds
+    case plants
+    
+    var id: AppScreen { self }
+}
+
+extension AppScreen {
+    
+    @ViewBuilder
+    var label: some View {
+        switch self {
+            case .backyard:
+                Label("Backyards", systemImage: "tree")
+            case .birds:
+                Label("Birds", systemImage: "bird")
+            case .plants:
+                Label("Plants", systemImage: "leaf")
+        }
+    }
+    
+    @ViewBuilder
+    var destination: some View {
+        switch self {
+        case .backyard:
+            Text("Backyards")
+        case .birds:
+            Text("Birds")
+        case .plants:
+            Text("Plants")
+        }
+    }
+}
+
+struct AppTabView: View {
+    @Binding var selection: AppScreen
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            ForEach(AppScreen.allCases) { screen in
+                screen.destination
+                    .tag(screen as AppScreen?)
+                    .tabItem { screen.label }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        AppTabView(selection: .constant(.backyard))
     }
 }
 
