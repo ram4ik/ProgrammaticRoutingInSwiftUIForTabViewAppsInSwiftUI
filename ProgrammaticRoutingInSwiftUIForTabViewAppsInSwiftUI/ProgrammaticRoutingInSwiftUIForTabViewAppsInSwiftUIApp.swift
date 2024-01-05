c9db4de1b96c92231e4095bd757e902371b4263e
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-05 10:10:26 +0200

    Initial Commit

---
+++ ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUI/ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUIApp.swift
+//  ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUI
+//
+//  Created by test on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticRoutingInSwiftUIForTabViewAppsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
